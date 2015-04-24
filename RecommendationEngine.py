70b37c565581bf350cd9fe540007eb5b776678e8
Isabella Li <dnflyangle@gmail.com>
2015-04-24 15:38:10 +1000

    use matrix to replace two layer dictionary to improve performance.

--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -3,149 +3,120 @@ from math import sqrt
 from operator import itemgetter
 import time
 import pprint
+import numpy
+from scipy.sparse import csr_matrix, dok_matrix
 
 class RecommendationEngine(object):
 
-	def __init__(self, inputfile, testingfile, k):
+	def __init__(self, inputfile, testingfile, userSize, movieSize):
 		self.__userDic = defaultdict(lambda: defaultdict(int))
-		self.__userDicSorted = defaultdict(list)
-		self.__similarityDict = defaultdict(lambda: defaultdict(float))
+		self.__similarityMatrix = None
+		self.__sortedIndexMatrix = None
 		self.__inputfile = inputfile
 		self.__testingfile = testingfile
-		self.__k = k
+		self.__userSize = userSize
+		self.__movieSize = movieSize
+		self.__userMovieMatrix = dok_matrix((userSize+1,movieSize+1))
+
+	def _processLine(self,line):
+		items = line.split("::")
+		userId = int(items[0])
+		movieId = int(items[1])
+		rating = int(items[2])
+
+		return userId, movieId, rating
 
 	def generateUserDic(self):
 		with open(self.__inputfile, 'r') as source:
 			for line in source:
-				items = line.split("::")
-				userId = int(items[0])
-				movieId = int(items[1])
-				rating = int(items[2])
+				userId, movieId, rating = self._processLine(line)
+
 				self.__userDic[userId][movieId] = rating
-
-	def sortUserDictByMovieId(self):
-		for key in self.__userDic:
-			self.__userDicSorted[key] = sorted(self.__userDic[key].items())
+				self.__userMovieMatrix[userId,movieId] = (rating - 3)
 		
+	def _generateNormMatrix(self, similarity_matrix):
+		square_mag = numpy.diag(similarity_matrix)
+		inv_square_mag = 1 / square_mag
+		inv_square_mag[numpy.isinf(inv_square_mag)] = 0
+
+		return numpy.diag(numpy.sqrt(inv_square_mag))
+
+	def _calculateCosineMatrix(self,similarity_matrix,norm_matrix):
+		cosine_matrix = similarity_matrix * norm_matrix
+		cosine_matrix = cosine_matrix.T * norm_matrix
+
+		return cosine_matrix
+
 	def calculateSimilarity(self):
-		adjustedUserDict = self._adjustValue(self.__userDicSorted)
+		similarity_matrix = self.__userMovieMatrix.dot(self.__userMovieMatrix.T).todense()
+		norm_matrix = self._generateNormMatrix(similarity_matrix)
 
-		userSize = len(adjustedUserDict)
+		self.__similarityMatrix = self._calculateCosineMatrix(similarity_matrix,norm_matrix)
 
-		for i in range(1, userSize+1):
-			for j in range(i+1, userSize+1):
-				similarity = self._calculatePairSimilarity(adjustedUserDict[i],adjustedUserDict[j])
-				self.__similarityDict[i][j] = similarity
-				self.__similarityDict[j][i] = similarity
+	def sortNeighbours(self):
+		self.__sortedIndexMatrix = self.__similarityMatrix.argsort()
 
-	def findTopKNeighbours(self):
-		for key in self.__similarityDict:
-			self.__similarityDict[key] = self._findTopKNeighboursForUser(self.__similarityDict[key])
+	def getTopKNeighbours(self, user, k):
+		topKNeighbours = self.__sortedIndexMatrix[user, self.__userSize-k:self.__userSize]
 
-	def evaluate(self):
+		return numpy.asarray(topKNeighbours).flatten()
+
+	def _calculateVoteScore(self, topKUsers, movieId):
+		voteScore = 0.0
+		count = 0
+		for user in topKUsers:
+			if movieId in self.__userDic[user]:
+				voteScore += self.__userDic[user][movieId]
+				count += 1
+
+		if count > 0:
+			voteScore /= count
+
+		return voteScore
+
+	def evaluate(self, k):
 		diff = 0.0
 		ratingCount = 0
-		zeroCount = 0
+
 		with open(self.__testingfile, 'r') as testingfile:
 			for line in testingfile:
-				items = line.split('::')
-				userId = int(items[0])
-				movieId = int(items[1])
-				rating = int(items[2])
-				topKUsers = self.__similarityDict[userId]
-				voteScore = 0.0
-				count = 0
-				for userSimPair in topKUsers:
-					if movieId in self.__userDic[userSimPair[0]]:
-						voteScore += self.__userDic[userSimPair[0]][movieId]
-						count += 1
-				if count > 0:
-					voteScore /= count
-				else:
-					zeroCount += 1
+				userId, movieId, rating = self._processLine(line)
+
+				topKUsers = self.getTopKNeighbours(userId, k)
+				
+				voteScore = self._calculateVoteScore(topKUsers, movieId)
 				diff += abs(voteScore - rating)
 				ratingCount += 1
+
 		diff /= ratingCount
-		print 'Average Error:', diff
-		print 'zeroCount:', zeroCount
-
-	def _adjustValue(self, userDic):
-		adjustUserDic = {}
-
-		for key in userDic:
-			adjustlist = []
-			for pair in userDic[key]:
-				adjustlist.append((pair[0],pair[1]-3))
-			adjustUserDic[key] = adjustlist
-
-		return adjustUserDic
-
-	def _calculatePairSimilarity(self, userI, userJ):
-
-		normProduct = self._calculateNorm(userI) * self._calculateNorm(userJ)
-
-		dotProduct = self._calculateDotProduct(userI,userJ)
-	
-		if dotProduct == 0:
-			return 0
-		else:
-			similarity = dotProduct/normProduct
-			return similarity
-
-	def _calculateNorm(self, user):
-		norm = 0.0
-		for pair in user:
-			norm += pair[1] * pair[1]
-		return sqrt(norm)
-
-	def _calculateDotProduct(self,userI,userJ):
-		i, j, dotProduct = 0, 0, 0.0
-		
-		while i < len(userI) and j < len(userJ):
-			if userI[i][0] < userJ[j][0]:
-				i += 1
-			elif userJ[j][0] < userI[i][0]:
-				j += 1
-			else:
-				dotProduct += userI[i][1] * userJ[j][1]
-				i += 1
-				j += 1
-
-		return dotProduct
-
-	def _findTopKNeighboursForUser(self, userDict):
-		sortedList = sorted(userDict.items(), key=itemgetter(1), reverse=True)
-
-		return sortedList[:self.__k]
-
-	def getSimilarityDict(self):
-		return self.__similarityDict
+		return diff
 
 def main():
-	engine = RecommendationEngine("data/training_1.txt", "data/testing_1.txt",300)
+	engine = RecommendationEngine("data/training.txt", "data/testing.txt", 6040, 3952)
 	start_time = time.time()
 	engine.generateUserDic()
 	generateEnd = time.time()
-	
-	engine.sortUserDictByMovieId()
-	sortEnd = time.time()
+
+	print "generateUserDic cost: " + str(generateEnd - start_time) + " secs"
 
 	engine.calculateSimilarity()
 	calculateSimilarityEnd = time.time()
 
-	engine.findTopKNeighbours()
-	findTopKNeighboursEnd = time.time()
+	print "calculateSimilarity cost: " + str(calculateSimilarityEnd - generateEnd) + " secs"
 
-	engine.evaluate()
+	engine.sortNeighbours()
+	sortNeighboursEnd = time.time()
+
+	print "sortNeighbours cost: " + str(sortNeighboursEnd - calculateSimilarityEnd) + " secs"
+
+	with open("evaluateResult.txt", 'w') as output:
+		for k in range(1,500):
+			if k % 10 == 0:
+				print "be more confident! K is :" + str(k) + " now!"
+				output.write("for k = " + str(k) + " error is : " + str(engine.evaluate(k))) + "\n"
 	evaluateEnd = time.time()
 
-	print "generateUserDic cost: " + str(generateEnd - start_time) + " secs"
-	print "sortUserDictByMovieId cost: " + str(sortEnd - generateEnd) + " secs"
-	print "calculateSimilarity cost: " + str(calculateSimilarityEnd - sortEnd) + " secs"
-	print "findTopKNeighbours cost: " + str(findTopKNeighboursEnd - calculateSimilarityEnd) + " secs"
-	print "evaluate cost" + str(evaluateEnd - findTopKNeighboursEnd) + " secs"
-	with open("similarityResult.txt",'w') as output:
-		output.write(pprint.pformat(engine.getSimilarityDict(),indent=4))
+	print "evaluate cost: " + str(evaluateEnd - sortNeighboursEnd) + " secs"
 
 
 if __name__ == '__main__':
